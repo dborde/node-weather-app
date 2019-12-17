8313057ad2031e12e97899cd42e55897d414e44e
David Borde <dborde@gogoair.com>
2019-12-16 18:24:39 -0600

    adding additional weather info reporting

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,5 +1,4 @@
 const request = require('request')
-
 const forecast = (longitude, latitude, callback) => {
   const url = `https://api.darksky.net/forecast/3fa13b849e0359902165ef0d437a43fe/${latitude},${longitude}`
   request({ url, json: true }, (error, { body }) => {
@@ -9,8 +8,10 @@ const forecast = (longitude, latitude, callback) => {
       callback('Unable to find location!', undefined)
     } else {
       const { temperature, precipProbability } = body.currently
-      const { summary } = body.daily.data[0]
-      callback(undefined, `${summary} It is currently degrees ${temperature} out. There is a ${precipProbability}% chance of rain.`)
+      const { summary, temperatureHigh, temperatureLow, windSpeed, windGust  } = body.daily.data[0]
+      callback(undefined, `${summary} The temperature is currently ${parseInt(temperature)} degrees with a high of ${parseInt(temperatureHigh)}
+      and a low of ${parseInt(temperatureLow)}.
+      There is a ${parseInt(precipProbability)}% chance of rain. Winds are at ${parseInt(windSpeed)} mph with gusts up to ${parseInt(windGust)} mph.`)
     }
   })
 }
